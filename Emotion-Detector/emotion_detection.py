be4a12d532ecdcd4bb0f2228a20ddd15a6686922
Marvin <mdengomg2@gmail.com>
2024-02-17 13:23:01 -0800

    feat: Add sentiment analyzer function and output formatter

---
+++ Emotion-Detector/emotion_detection.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def sentiment_analyzer(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, json=input_json, headers=headers)
+    if response.status_code == 200:
+        return response.json()['text']
+    else:
+        return "Error: Failed to get a response from the server."
+
+
+def format_output(response_json):
+    emotions = response_json['emotionPredictions'][0]['emotion']
+    anger_score = emotions.get('anger', 0)
+    disgust_score = emotions.get('disgust', 0)
+    fear_score = emotions.get('fear', 0)
+    joy_score = emotions.get('joy', 0)
+    sadness_score = emotions.get('sadness', 0)
+
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant_emotion
+    return emotion_scores
+
+
+if __name__ == "__main__":
+    string = "I love this new technology"
+    print(sentiment_analyzer(string))

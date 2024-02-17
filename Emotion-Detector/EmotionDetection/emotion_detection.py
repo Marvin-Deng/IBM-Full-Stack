8007b6884b6c6a17758903e7f95ca04653a95c49
Marvin <mdengomg2@gmail.com>
2024-02-17 13:34:11 -0800

    feat: Add tests for checking dominant_emotion from EmotionDetection

--- Emotion-Detector/EmotionDetection/emotion_detection.py
+++ Emotion-Detector/EmotionDetection/emotion_detection.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def sentiment_analyzer(text_to_analyze):
+def emotion_predictor(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
